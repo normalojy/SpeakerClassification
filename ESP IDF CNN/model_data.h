e0333fb787a5b03c942e235d0c42099d390e21e8
normalojy <137070967+normalojy@users.noreply.github.com>
2026-07-29 16:25:17 +0800

    Add files via upload

---
+++ ESP IDF CNN/model_data.h
@@ -0,0 +1,7 @@
+#ifndef TENSORFLOW_LITE_MICRO_CNN_SPEAKER_IDENTIFICATION_MODEL_H_
+#define TENSORFLOW_LITE_MICRO_CNN_SPEAKER_IDENTIFICATION_MODEL_H_
+
+extern const unsigned char tensorflowlite_cnn_tflite[];
+extern const unsigned int tensorflowlite_cnn_tflite_len;
+
+#endif  // TENSORFLOW_LITE_MICRO_CNN_SPEAKER_IDENTIFICATION_MODEL_H_
